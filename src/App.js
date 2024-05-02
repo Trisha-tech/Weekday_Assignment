e3a356d7de8d43e3a7029971e01ed0417d82c567
Trisha-tech <1234trishasahu@gmail.com>
2024-05-02 10:37:55 +0530

    Default Filter's Added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import './Home'
 import JobCard from './Home';
+import Filter from './components/Filter'
 
 function App() {
   return (
     <div className="App">
+         <Filter/>
      <JobCard/>
-     
+  
     </div>
   );
 }
